015dbcdb4625cb3d3f4d53b36351dea8c292f7d1
Methminpulsara <methminpulsara10@gmail.com>
2025-05-22 16:39:50 +0530

    tailwind intigration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
-  return (
-    <h1>Header</h1>
-  )
-}
 
-export default Header
+
+
+  return (
+    <>
+<div>
+  <div className="bg-sky-50 ">hy</div>
+  <div className="bg-sky-100">hy</div>
+  <div className="bg-sky-200">hy</div>
+  <div className="bg-sky-300">hy</div>
+  <div className="bg-sky-400">hy</div>
+  <div className="bg-sky-500">hy</div>
+  <div className="bg-sky-600">hy</div>
+  <div className="bg-sky-700">hy</div>
+  <div className="bg-sky-800">hy</div>
+  <div className="bg-sky-900">hy</div>
+  <div className="bg-sky-950">hy</div>
+  <button class="ps-3 p-1 pe-3 m-1 rounded-4xl bg-violet-500 hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700 ...">
+  Save 
+</button>
+</div>  
+
+  </>
+  );
+};
+
+export default Header;
