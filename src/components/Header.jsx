0b2b83741df621e8616a98e27aa28a434eccb70d
Methminpulsara <methminpulsara10@gmail.com>
2025-05-22 23:16:15 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
 
 
+//veriable eke state eka wens krnn 
+let [count,setCount] =useState(0);
+
+function countplus(){
+  setCount(count++)
+  console.log(count)
+}
 
   return (
     <>
-<div>
-  <div className="bg-sky-50 ">hy</div>
-  <div className="bg-sky-100">hy</div>
-  <div className="bg-sky-200">hy</div>
-  <div className="bg-sky-300">hy</div>
-  <div className="bg-sky-400">hy</div>
-  <div className="bg-sky-500">hy</div>
-  <div className="bg-sky-600">hy</div>
-  <div className="bg-sky-700">hy</div>
-  <div className="bg-sky-800">hy</div>
-  <div className="bg-sky-900">hy</div>
-  <div className="bg-sky-950">hy</div>
-  <button class="ps-3 p-1 pe-3 m-1 rounded-4xl bg-violet-500 hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700 ...">
-  Save 
-</button>
-</div>  
-
-  </>
+      <span>
+        <h1>React with Vite</h1>
+        <h2>{count}</h2><br />
+        <button onClick={countplus}>click</button>
+      </span>
+    </>
   );
 };
 
